e1dc67303007b54747549881cdfd9f156fd2db3c
Felix Heck <hi@whotheheck.de>
2018-10-11 17:52:01 +0200

    fix timezone handling by switching cron module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cron = require('node-cron')
+const Cron = require('cron').CronJob
 const environment = require('./lib/environment')
 const handlers = require('./lib/handlers')
 require('dotenv').config()
@@ -8,6 +8,7 @@ require('dotenv').config()
 environment.validate()
 
 const app = express()
+const crons = {}
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,7 +20,7 @@ app.post('/folks', handlers.feedback)
 Object.keys(environment.timezones).forEach(timezone => {
   const cities = environment.timezones[timezone].map(x => x.toLowerCase())
 
-  cron.schedule('30 11 * * MON-FRI', handlers.match(cities), { timezone })
+  crons[timezone] = new Cron('30 11 * * MON-FRI', handlers.match(cities), null, true, timezone)
 })
 
 app.listen(environment.port, () => console.log('server listening'))
