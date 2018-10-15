cf9e36c1ae287271ee62c9bb054677278e5b1c3b
Felix Heck <hi@whotheheck.de>
2018-10-15 18:13:35 +0200

    add initial mongo setup

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const Cron = require('cron').CronJob
 const environment = require('./lib/environment')
 const handlers = require('./lib/handlers')
 const gatekeeper = require('./lib/gatekeeper')
+const storage = require('./lib/storage')
 require('dotenv').config()
 
 environment.validate()
+storage.init()
 
 const app = express()
 const crons = {}
@@ -14,12 +16,16 @@ const rawBodySaver = (req, res, buf, encoding) => {
   req.rawBody = buf && buf.length ? buf.toString(encoding || 'utf8') : undefined
 }
 
+const asyncMiddleware = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next)
+}
+
 app.use(bodyParser.json({ verify: rawBodySaver }))
 app.use(bodyParser.urlencoded({ extended: true, verify: rawBodySaver }))
 
 app.get('/', (req, res) => res.json({ status: 'ok' }))
-app.post('/commands', gatekeeper.lock, handlers.commands)
-app.post('/actions', gatekeeper.lock, handlers.actions)
+app.post('/commands', gatekeeper.lock, asyncMiddleware(handlers.commands))
+app.post('/actions', gatekeeper.lock, asyncMiddleware(handlers.actions))
 
 Object.keys(environment.timezones).forEach(timezone => {
   const cities = environment.timezones[timezone].map(x => x.toLowerCase())
