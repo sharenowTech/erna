b2837f20983d65ffc6a63f4b4492f4729a86aef5
Felix Heck <hi@whotheheck.de>
2018-10-16 15:36:17 +0200

    add feature to define match day

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const env = require('./lib/env')
 const middleware = require('./lib/middleware')
 const handlers = require('./lib/handlers')
 const gatekeeper = require('./lib/gatekeeper')
+const scheduler = require('./lib/scheduler')
 const storage = require('./lib/storage')
 
 storage.init()
 const app = express()
-const crons = {}
 
 app.use(bodyParser.json({ verify: middleware.rawBody }))
 app.use(bodyParser.urlencoded({ extended: true, verify: middleware.rawBody }))
@@ -21,9 +21,11 @@ app.post('/commands', gatekeeper.lock, middleware.async(handlers.commands))
 app.post('/actions', gatekeeper.lock, middleware.async(handlers.actions))
 
 Object.keys(env.locations.tzs).forEach(timezone => {
-  const cities = env.locations.tzs[timezone].map(x => x.toLowerCase())
+  const cities = env.locations.tzs[timezone]
+  const cronPattern = `${env.matchTime.cron} * * ${env.matchDay.raw}`
+  const cron = new Cron(cronPattern, handlers.match(cities), null, true, timezone)
 
-  crons[timezone] = new Cron(`${env.matchTime.cron} * * MON-FRI`, handlers.match(cities), null, true, timezone)
+  scheduler.add(timezone, cron)
 })
 
 app.listen(env.port, () => console.log('server listening'))
